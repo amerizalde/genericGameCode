9b1c7b0fd678f372081b6c94a9b99b9cc735412e
Andrew Merizalde <andrewmerizalde@hotmail.com>
2014-05-12 17:52:06 -0600

    add 4-5 rule

--- visualaid_2dmaps.py
+++ visualaid_2dmaps.py
@@ -31,6 +31,145 @@ class Dungeon(object):
             print " {},{}  ".format(x, y)
             n -= 1
 
+    def rule_four_five(self, n):
+        """ Use after performing a .walk() or .visualize() to smooth
+        out the noise. Increase N for more open space.
+        """
+        w, h, _ = self.grid.shape
+        tile = [1, 128, 255]
+        wall = [0, 0, 0]
+        while n:
+            for i in xrange(w):
+                for j in xrange(h):
+                    neighbors = self.num_walls(i, j)
+                    if neighbors <= 3:
+                        self.grid[i, j] = tile
+                    elif neighbors > 5:
+                        self.grid[i, j] = wall
+            # iterate
+            n -= 1
+
+    def num_walls(self, i, j):
+        """ determine the number of neighbors that are a wall.
+                WARNING: WALL OF TEXT INCOMING
+        """
+        # nw
+        def northw(grid, i, j):
+            cell = grid[i - 1, j - 1]
+            print cell
+            return True if cell == [0, 0, 0] else False
+        # n
+        def north(grid, i, j):
+            cell = grid[i, j - 1]
+            print cell
+            return True if cell == [0, 0, 0] else False
+        # ne
+        def northe(grid, i, j):
+            cell = grid[i + 1, j - 1]
+            print cell
+            return True if cell == [0, 0, 0] else False
+        # e
+        def east(grid, i, j):
+            cell = grid[i + 1, j]
+            print cell
+            return True if cell == [0, 0, 0] else False
+        # se
+        def southe(grid, i, j):
+            cell = grid[i + 1, j + 1]
+            print cell
+            return True if cell == [0, 0, 0] else False
+        # s
+        def south(grid, i, j):
+            cell = grid[i, j + 1]
+            print cell
+            return True if cell == [0, 0, 0] else False
+        # sw
+        def southw(grid, i, j):
+            cell = grid[i - 1, j + 1]
+            print cell
+            return True if cell == [0, 0, 0] else False
+        # w
+        def west(grid, i, j):
+            cell = grid[i - 1, j]
+            print cell
+            return True if cell == [0, 0, 0] else False
+
+        count = 0
+
+        # edge cases
+        if i == 0 and j == 0:
+            # TOP LEFT CORNER has 3 possible neighbors
+            # e
+            if east(self.grid, i, j): count += 1
+            # se
+            if southe(self.grid, i, j): count += 1
+            # s
+            if south(self.grid, i, j): count += 1
+        elif i == 0 and j == h:
+            # BOTTOM LEFT CORNER has 3 possible neighbors
+            # e
+            if east(self.grid, i, j): count += 1
+            # ne
+            if northe(self.grid, i, j): count += 1
+            # n
+            if north(self.grid, i, j): count += 1
+        elif i == w and j == 0:
+            # TOP RIGHT CORNER has 3 possible neighbors
+            # w
+            if west(self.grid, i, j): count += 1
+            # sw
+            if southw(self.grid, i, j): count += 1
+            # s
+            if south(self.grid, i, j): count += 1
+        elif i == w and j == h:
+            # BOTTOM RIGHT CORNER has 3 possible neighbors
+            # w
+            if west(self.grid, i, j): count += 1
+            # nw
+            if northw(self.grid, i, j): count += 1
+            # n
+            if north(self.grid, i, j): count += 1
+        elif i == 0:
+            # LEFT SIDE has 5 possible neighbors
+            if north(self.grid, i, j): count += 1
+            if northe(self.grid, i, j): count += 1
+            if east(self.grid, i, j): count += 1
+            if southe(self.grid, i, j): count += 1
+            if south(self.grid, i, j): count += 1
+        elif i == w:
+            # RIGHT SIDE has 5 possible neighbors
+            if north(self.grid, i, j): count += 1
+            if northw(self.grid, i, j): count += 1
+            if west(self.grid, i, j): count += 1
+            if southw(self.grid, i, j): count += 1
+            if south(self.grid, i, j): count += 1
+            pass
+        elif j == 0:
+            # TOP SIDE has 5 possible neighbors
+            if west(self.grid, i, j): count += 1
+            if southw(self.grid, i, j): count += 1
+            if south(self.grid, i, j): count += 1
+            if southe(self.grid, i, j): count += 1
+            if east(self.grid, i, j): count += 1
+        elif j == h:
+            # BOTTOM SIDE has 5 possible neighbors
+            if west(self.grid, i, j): count += 1
+            if northw(self.grid, i, j): count += 1
+            if north(self.grid, i, j): count += 1
+            if northe(self.grid, i, j): count += 1
+            if east(self.grid, i, j): count += 1
+        else:
+            # everything else has 8 possible neighbors
+            if north(self.grid, i, j): count += 1
+            if northw(self.grid, i, j): count += 1
+            if west(self.grid, i, j): count += 1
+            if southw(self.grid, i, j): count += 1
+            if south(self.grid, i, j): count += 1
+            if southe(self.grid, i, j): count += 1
+            if east(self.grid, i, j): count += 1
+            if northe(self.grid, i, j): count += 1
+        return count
+
     def toImage(self, filename):
         try:
             self.surface = pygame.surfarray.make_surface(self.grid)
@@ -95,5 +234,6 @@ if __name__ == "__main__":
     _, w, h = argv
     w, h = int(w), int(h)
     dungeon = Dungeon(w, h)
-    dungeon.visualize(32768)
+    dungeon.walk(32768)
+    dungeon.rule_four_five(3)
     dungeon.toImage("test_{}.png".format(random.randint(1000, 2000)))
